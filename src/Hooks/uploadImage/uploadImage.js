71e5d9d09f37c9a4a621124f649f88283a473479
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-06-09 12:30:09 +0600

    handler added and form added

--- src/Hooks/uploadImage/uploadImage.js
+++ src/Hooks/uploadImage/uploadImage.js
@@ -2,5 +2,10 @@ export const imageUpload = async image => {
     const formData = new formData()
     formData.append('image', image)
     const url = `https//api.imgdb.com.1.updated/key=${key}`
-    const respone = await fetch(url)
+    const response = await fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+    const data = await response.json()
+    return data;
 }
--- src/Page/Classes/ClassInfo/ClassInfo.jsx
+++ src/Page/Classes/ClassInfo/ClassInfo.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 const ClassInfo = ({ item }) => {
     console.log(item)
     const { name, image, price } = item;
+    const handleAddClass = item => {
+        console.log(item)
+    }
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -11,7 +14,7 @@ const ClassInfo = ({ item }) => {
                 <h2 className="card-title">{price}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button onClick={() => handleAddClass(item)} className="btn btn-primary">Buy Now</button>
                 </div>
             </div>
         </div>
