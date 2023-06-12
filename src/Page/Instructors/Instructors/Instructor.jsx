62027a07a2d3fce34aaaa4bffbbe703c95204588
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-06-12 15:05:28 +0600

    set animation

--- src/Page/Instructors/Instructors/Instructor.jsx
+++ src/Page/Instructors/Instructors/Instructor.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from "framer-motion"
 
 const Instructor = ({ item }) => {
     const { _id, name, image, email, numberOfClass } = item;
@@ -8,15 +9,16 @@ const Instructor = ({ item }) => {
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
-            <div className="card-body">
+            <motion.div animate={{ y: 10 }}
+                transition={{ ease: "easeOut", duration: 2 }} className="card-body">
                 <h2 className="card-title">Instructor Name: {name}</h2>
                 <h2 className="card-title">Email: {email}</h2>
                 <h2 className="card-title">Available Seats: {numberOfClass}</h2>
                 <div className="card-actions justify-end">
                     <Link to={`/class/${_id}`} className='w-full' > <button className="btn btn-primary w-full mt-5">See classes</button></Link>
                 </div>
-            </div>
-        </div>
+            </motion.div>
+        </div >
     );
 };
 
