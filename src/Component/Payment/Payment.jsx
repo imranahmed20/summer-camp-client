d2ae2ed34864b63b169a70f732f6c8fcd573f5e9
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-06-13 11:21:22 +0600

    payment added

--- src/Component/Payment/Payment.jsx
+++ src/Component/Payment/Payment.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import CheckOut from './CheckOut';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
+
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 const Payment = () => {
     return (
-        <div>
-            This is payment
+        <div className='w-full p-10'>
+            <h1 className='text-center text-3xl font-bold text-red-500 mb-10'>Please Pay</h1>
 
+            <Elements stripe={stripePromise}>
+                <CheckOut></CheckOut>
+            </Elements>
         </div>
     );
 };
